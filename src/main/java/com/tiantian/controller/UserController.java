dbd85a6543bc490437214a1dab04a18a935a9cba
tiantian <tianpwaydn@163.com>
2019-08-18 20:33:22 +0800

    缓存注解自调用失效

--- src/main/java/com/tiantian/controller/UserController.java
+++ src/main/java/com/tiantian/controller/UserController.java
@@ -27,6 +27,7 @@ public class UserController {
 
     /**
      * condition和unless的区别，condition对传入值生效，unless对结果result生效
+     * 缓存注解在同一层的调用不生效 如: 直接在同一层调用方法 selectByUserId(5); 缓存不生效
      * @param userId
      * @return
      */
